c1cbbd204a4ba571e43af71e8b4358f58bb63c5f
srymh <31023018+srymh@users.noreply.github.com>
2026-01-29 02:00:46 +0900

    chore(main): 不要なNodeJS環境変数の定義を削除

--- electron/electron-env.d.ts
+++ electron/electron-env.d.ts
@@ -1,28 +1,5 @@
 /// <reference types="vite-plugin-electron/electron-env" />
 
-declare namespace NodeJS {
-  interface ProcessEnv {
-    /**
-     * The built directory structure
-     *
-     * ```tree
-     * ├─┬─┬ dist
-     * │ │ └── index.html
-     * │ │
-     * │ ├─┬ dist-electron
-     * │ │ ├─┬ main
-     * │ │ │ └── index.js
-     * │ │ └─┬ preload
-     * │ │   └── index.mjs
-     * │
-     * ```
-     */
-    APP_ROOT: string
-    /** /dist/ or /public/ */
-    VITE_PUBLIC: string
-  }
-}
-
 // Used in Renderer process, expose in `preload/index.ts`
 interface Window {
   api: import('#/main/ipc/electronApi').ElectronApi
