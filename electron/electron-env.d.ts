d01c5045cb940ee2fbe4fa3c4ba7ce382e1f897a
srymh <31023018+srymh@users.noreply.github.com>
2026-04-21 23:46:14 +0900

    chore: Electron の型宣言参照を整理し版を更新 (#31)
    - vite-plugin-electron の reference types を削除
    - パッケージの version を 0.1.0-alpha.2 に更新

--- electron/electron-env.d.ts
+++ electron/electron-env.d.ts
@@ -1,5 +1,3 @@
-/// <reference types="vite-plugin-electron/electron-env" />
-
 // Used in Renderer process, expose in `preload/index.ts`
 interface Window {
   api: import('#/main/ipc/electronApi').ElectronApi
